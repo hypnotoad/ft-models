ca9e2048b1b9b1fda445d5c9b32e26e5e2232c69
Ralf Dragon <hypnotoad@lindra.de>
2024-03-29 16:14:50 +0100

    clean up

--- packages/plotter/edge.py
+++ packages/plotter/edge.py
@@ -1,46 +1,60 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 
-thr1 = 100
-thr2 = 200
+class Tracer:
+    def __init__(self, grayscale, thr1 = 100, thr2 = 200):
+        self.thr1 = thr1
+        self.thr2 = thr2
+        self.img = grayscale
 
-img = cv.imread('face.jpg', cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-edges = cv.Canny(img, thr1, thr2)
+    def extract_contours(self):
+        edges = cv.Canny(self.img, self.thr1, self.thr2)
 
-contours, hierarchy = cv.findContours(edges, cv.RETR_LIST,
-                                 cv.CHAIN_APPROX_TC89_L1)
+        contours, hierarchy = cv.findContours(edges, cv.RETR_LIST,
+                                              cv.CHAIN_APPROX_TC89_L1)
+        return contours, edges
 
-sum_length = sum([len(x) for x in contours])
-mean_length = sum_length / len(contours)
-print("{} contours were extracted with a total of {} points".
-      format(len(contours), sum_length))
+    def draw_contours(self, contours, img):
+        cv.drawContours(img, contours, -1, (0, 0, 0), 1)
+        
 
-longcontours = [x for x in contours if len(x) > mean_length]
-print("{} contours are longer than the mean {}".
-      format(len(longcontours), mean_length))
+if __name__ == "__main__":
+    img = cv.imread('face.jpg', cv.IMREAD_GRAYSCALE)
+    assert img is not None
 
-cimg  = np.full(img.shape, 255, dtype=img.dtype)
-cimg2 = np.full(img.shape, 255, dtype=img.dtype)
-cv.drawContours(cimg, contours, -1, (0, 0, 0), 2)
-cv.drawContours(cimg2, longcontours, -1, (0, 0, 0), 2)
+    t = Tracer(img)
 
-plt.subplot(221)
-plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    contours, edges = t.extract_contours()
+    sum_length = sum([len(x) for x in contours])
+    mean_length = sum_length / len(contours)
+    print("{} contours were extracted with a total of {} points".
+          format(len(contours), sum_length))
 
-plt.subplot(222)
-plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    longcontours = [x for x in contours if len(x) > mean_length]
+    print("{} contours are longer than the mean {}".
+          format(len(longcontours), mean_length))
 
-plt.subplot(223)
-plt.imshow(cimg,cmap = 'gray')
-plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
+    cimg  = np.full(img.shape, 255, dtype=img.dtype)
+    cimg2 = np.full(img.shape, 255, dtype=img.dtype)
+    t.draw_contours(contours, cimg)
+    t.draw_contours(longcontours, cimg2)
 
-plt.subplot(224)
-plt.imshow(cimg2,cmap = 'gray')
-plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
+    from matplotlib import pyplot as plt
+    plt.subplot(221)
+    plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(222)
+    plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(223)
+    plt.imshow(cimg,cmap = 'gray')
+    plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(224)
+    plt.imshow(cimg2,cmap = 'gray')
+    plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
 
 
-plt.show()
+    plt.show()
