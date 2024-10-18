864cd6116b0408704d029c5ab8430e216821c1b7
Ralf Dragon <hypnotoad@lindra.de>
2024-10-18 13:02:12 +0200

    Add class for the camera calibration

---
+++ packages/navigator/calibration.py
@@ -0,0 +1,40 @@
+import json
+import numpy
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return super().default(obj)
+
+class Calibration():
+    def __init__(self, C=None, dist=None):
+        self.C = C
+        self.dist = dist
+        
+    def load(self, filename):
+        with open(filename, "r") as f:
+            loaded = json.load(f)
+        self.C = numpy.asarray(loaded["camera_matrix"])
+        self.dist = numpy.asarray(loaded["distortion"])
+        
+    def save(self, filename):
+        calibration = {"camera_matrix": self.C,
+                       "distortion": self.dist}
+        with open(filename, "w") as f:
+            json.dump(calibration, f, cls=NumpyEncoder)
+
+    def getCameraMatrix(self):
+        return self.C
+
+    def getDistortion(self):
+        return self.dist
+
+    def valid(self):
+        return self.C is not None and self.dist is not None
+
+if __name__ == "__main__":
+    c = Calibration()
+    c.load("calibration.json")
+    print("{}\n{}\n{}".format(c.valid(), c.getCameraMatrix(), c.getDistortion()))
+    
