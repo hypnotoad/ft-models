467b5c8c48f61e04dcf18bbb15ee87defcddd216
Ralf Dragon <hypnotoad@lindra.de>
2024-10-18 10:49:41 +0200

    Create board as well

--- packages/navigator/generate_markers.py
+++ packages/navigator/generate_markers.py
@@ -9,3 +9,7 @@ for id in range(10):
     
     img = dictionary.drawMarker(id, size)
     cv2.imwrite("img-%03d.png" % id, img)
+
+board = cv2.aruco.CharucoBoard.create(5, 3, 10, 5, dictionary)
+img = board.draw((1920, 1080))
+cv2.imwrite("board.png", img)
