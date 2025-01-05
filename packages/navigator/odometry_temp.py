6bb4d503563a23a3cfda5a2f2b6d0d3fb39865f0
Ralf Dragon <hypnotoad@lindra.de>
2025-01-05 15:03:14 +0100

    Allow longer motions

--- packages/navigator/odometry_temp.py
+++ packages/navigator/odometry_temp.py
@@ -13,7 +13,7 @@ class OdometryTemp:
 
             self.speed = 300
             self.min_duration = 0.2
-            self.max_duration = 1
+            self.max_duration = 2
 
             # calibrated empirical at 300:
             self.distances_per_second = (10, None, 12.5)
@@ -31,19 +31,25 @@ class OdometryTemp:
             self.txt.SyncDataEnd()
 
         def compute_speeds_and_duration(self, angle_dist):
-            def add_component(measurement, target, index):
-                retval = numpy.array([[0, 0,  0]])
-                offset = target - measurement
-                if abs(offset) > self.thresholds[index]:
-                    retval[0][index] = 1 if offset < 0 else -1
-                return retval
-
             move = numpy.array([[0, 0, 0]]) #fwd, right, turn
-            move += add_component(angle_dist["hori_angle"], target=0, index=2)
-            move += add_component(angle_dist["dist_cm"], target=100, index=0)
+            duration = self.max_duration
+
+            def add_component(measurement, target, index):
+                nonlocal move, duration
+                
+                motion_component = numpy.array([[0, 0,  0]])
+                offset = target - measurement
+                factor = abs(offset) / self.thresholds[index]
+                if factor > 1:
+                    motion_component[0][index] = 1 if offset < 0 else -1
+                    computed_duration = abs(offset) / self.distances_per_second[index]
+                    duration = min([self.max_duration, computed_duration/2, duration])
+                move += motion_component
+
+            add_component(angle_dist["hori_angle"], target=0, index=2)
+            add_component(angle_dist["dist_cm"], target=100, index=0)
 
             dist = move.dot(self.motions)
-            duration = self.min_duration
             return (dist, duration)
 
 
@@ -57,12 +63,15 @@ if __name__ == "__main__":
     def create_input(dist, angle):
         return {"hori_angle": angle, "dist_cm": dist}
 
-    angle_dist = create_input(110, 0) # 10 forwards
-    dist, duration = odo.compute_speeds_and_duration(angle_dist)
-    print("{} for {}s".format(dist, duration))
+    distances = [(110, 0), (90, 0), (100, 45), (100, -45)]
 
-    odo.set_motors(dist)
-    time.sleep(duration)
-    odo.set_motors(None)
+    for distance in distances:
+        angle_dist = create_input(distance[0], distance[1])
+        dist, duration = odo.compute_speeds_and_duration(angle_dist)
+        print("{} for {}s".format(dist, duration))
+
+        odo.set_motors(dist)
+        time.sleep(duration)
+        odo.set_motors(None)
 
     time.sleep(1)
